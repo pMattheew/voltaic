d446cebd651f678e1778a6c3051e8dcc28e37615
Patrick M <patrickmatias.silva@hotmail.com>
2024-02-22 22:53:34 +0000

    that cool logo is being displayed

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        VoltaicCLI v = new();
+        v.show();
     }
 }
---
+++ VoltaicCLI.cs
@@ -0,0 +1,15 @@
+class VoltaicCLI
+{
+    const string CoolLogo = @" __      __   _ _        _      
+ \ \    / /  | | |      (_)     
+  \ \  / /__ | | |_ __ _ _  ___ 
+   \ \/ / _ \| | __/ _` | |/ __|
+    \  / (_) | | || (_| | | (__ 
+     \/ \___/|_|\__\__,_|_|\___|
+                                
+                                ";
+    public void show()
+    {
+        Console.WriteLine(CoolLogo);
+    }
+}
